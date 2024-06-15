16e4e3617058de3524ec7cb2e44c2ad745affb10
SinghShubham1308 <shubhusingh1308@gmail.com>
2024-06-15 22:50:47 +0530

    Signed-off-by: SinghShubham1308 <shubhusingh1308@gmail.com>

---
+++ Sevlet/Servlets/src/main/java/com/servlet/LoginServlet.java
@@ -0,0 +1,29 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login",name = "login")
+
+public class LoginServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 737025133711952703L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("servlet working");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>servlet working</h1>");
+//		super.doGet(req, resp);
+	}
+}
